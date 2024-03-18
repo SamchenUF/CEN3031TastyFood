c3b7525a8ae9a18aa9f10461430cf30c6de227aa
Sam Chen <76010610+SamchenUF@users.noreply.github.com>
2024-03-18 18:21:57 -0400

    Changed nav bar style and added folder for generate recipes page

--- tastyfoodapp/src/components/navbar.js
+++ tastyfoodapp/src/components/navbar.js
@@ -1,27 +1,54 @@
 import Link from 'next/link';
-
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/">
+                  <p>Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <p>About</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact">
+                  <p>Contact</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </>
   );
 };
+
+// const Navbar = () => {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link href="/">
+//             <p>Home</p>
+//           </Link>
+//         </li>
+//         <li>
+//           <Link href="/about">
+//             <p>About</p>
+//           </Link>
+//         </li>
+//         <li>
+//           <Link href="/contact">
+//             <p>Contact</p>
+//           </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
 
 export default Navbar;
