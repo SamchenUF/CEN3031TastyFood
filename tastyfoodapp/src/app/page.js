2922baac693be612c7ec785fbbdfb217f9c03860
wendyt0 <wendyto@ufl.edu>
2024-03-18 23:41:45 -0400

    fixing spacing for overall apperance of the page

--- tastyfoodapp/src/app/page.js
+++ tastyfoodapp/src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p className="fixed left-0 top-0 flex w-full justify-center"> Welcome to TastyFood</p>
+        <p className="fixed left-0 top-0 flex w-full justify-center mt-1 md:mt-4"> Welcome to TastyFood</p>
         <div>
           <p> Tastyfood will allow you to generate a variety of different recipes using AI tailored to your preferences and needs</p>
           <div class="flex justify-center">
