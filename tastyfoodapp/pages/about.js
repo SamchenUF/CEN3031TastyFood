96ff44f46fc96067005a522a24650fc4a18e7a89
Andrew Tang <115107511+andrtang@users.noreply.github.com>
2024-03-20 13:53:00 -0400

    added contact and about pages but they dont link from navbar

---
+++ tastyfoodapp/pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1 className="text-3xl font-bold">About Page</h1>
+        <p>This is the About page content.</p>
+      </main>
+    );
+  };
+
--- tastyfoodapp/pages/genrecipes.js
+++ tastyfoodapp/pages/genrecipes.js
@@ -1,12 +1,8 @@
-import React from 'react';
-
-const GenRecipes = () => {
+export default function GenRecipes() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-3xl font-bold">Generate Recipes Page</h1>
-      <p>This is the Generate Recipes page content.</p>
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1 className="text-3xl font-bold">Generate Recipes Page</h1>
+        <p>This is the Generate Recipes page content.</p>
+      </main>
   );
 };
-
-export default GenRecipes;
