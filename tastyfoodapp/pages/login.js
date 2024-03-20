ed818cd9550fd874a20c116f929a240583c54f0d
yyujinparkk <yujin23841@gmail.com>
2024-03-20 11:38:51 -0400

    got linked pages to work

---
+++ tastyfoodapp/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold">Login Page</h1>
+      <p>This is the Login page content.</p>
+    </main>
+  );
+};
+
+export default Login;
