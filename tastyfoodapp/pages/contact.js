552bfaefd0f68af8ba78fe491742be43a2c283cd
Andrew Tang <andrtang@hotmail.com>
2024-03-28 22:17:34 -0400

    navbar added to all pages

--- tastyfoodapp/pages/contact.js
+++ tastyfoodapp/pages/contact.js
@@ -1,9 +1,13 @@
+import Layout from "@/components/layout";
+
 export default function Contact() {
     return (
+      <Layout>
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
           <h1 className="text-3xl font-bold">Contact Recipes Page</h1>
           <p>This is the Contact page content.</p>
         </main>
+      </Layout>
     );
   };
   
---
+++ tastyfoodapp/pages/friends/hub.js
@@ -0,0 +1,13 @@
+import Layout from '@/components/layout';
+
+export default function Hub() {
+    return (
+    <Layout> 
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <h1 className="text-3xl font-bold">Friend Page</h1>
+            <p>This is the Friend page content.</p>
+        </main>
+    </Layout>
+    );
+  };
+
