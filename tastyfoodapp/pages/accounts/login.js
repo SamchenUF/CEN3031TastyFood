20e4e41acd8e339ac7aef39faedaac075b045e49
wendyt0 <wendyto@ufl.edu>
2024-04-24 14:44:55 -0400

    fixed save recipe button and outher ui

--- tastyfoodapp/pages/accounts/login.js
+++ tastyfoodapp/pages/accounts/login.js
@@ -72,7 +72,7 @@ const Login = () => {
           <a href="/accounts/reset">Forgot Password</a>
             <button
               type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
               Sign In
             </button>
